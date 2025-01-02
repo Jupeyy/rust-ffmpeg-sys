80ad3b11b2a0308a0985fbbef2956b9df1340d70
Jupeyy <jupjopjap@gmail.com>
2025-01-02 13:02:48 +0100

    f

--- build.rs
+++ build.rs
@@ -362,6 +362,9 @@ fn build() -> io::Result<()> {
     // configure misc build options
     enable!(configure, "BUILD_PIC", "pic");
 
+    let mut configure = Command::new("sh");
+    configure.arg("--version");
+
     // run ./configure
     let output = configure
         .output()
